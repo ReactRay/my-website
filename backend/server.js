b6fd51ad3d32aae52a9ea6152c99c63a50cd45af
ReactRay <raydiaz1899@gmail.com>
2025-03-24 18:55:27 +0200

    backend stuff

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,19 @@
 import express from 'express'
+import cors from 'cors'
 
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send('hello world')
-})
+const corsOptions = {
+  origin: ['http://127.0.0.1:5174', 'http://localhost:5174'],
+  credentials: true,
+}
 
-app.listen(3000, () => {
-  console.log('Server running on http://localhost:3000')
+app.use(cors(corsOptions))
+app.use(express.json())
+app.use(express.static('public'))
+
+const PORT = process.env.PORT || 3030
+
+app.listen(PORT, () => {
+  console.log('Up and running on ' + `http://localhost:${PORT}`)
 })
