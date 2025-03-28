b949395c7674578e4c4e2572db136848dda143b4
ReactRay <raydiaz1899@gmail.com>
2025-03-29 02:24:50 +0300

    change resume

--- frontend/src/Pages/Hero.jsx
+++ frontend/src/Pages/Hero.jsx
@@ -2,6 +2,7 @@ import { ProfileImage } from "../cmps/ProfileImage"
 import { Typewriter } from 'react-simple-typewriter'
 import ParticleBackground from "../cmps/ParticleBackground.jsx"
 import { showSuccessMsg } from "../services/event-bus.service.js"
+import { Link } from "react-scroll"
 export function Hero() {
     const props = {
         words: ['Web Developer', 'Tutor', 'YouTuber'],
@@ -20,8 +21,8 @@ export function Hero() {
                     <h1>hello ,im Radwan <hr /> <Typewriter {...props} /></h1>
                     <h3>welcome to my website</h3>
                     <div className="link-box">
-                        <a href="/Radwan_Mansur_Resume.pdf" download="Radwan_Mansur_CV.pdf">Download CV</a>
-                        <a href="">Projects</a>
+                        <a href="/Radwan_Mansur_CV.pdf" download="Radwan_Mansur_CV.pdf">Download CV</a>
+                        <Link to="projects">Projects</Link>
                     </div>
                 </div>
 
