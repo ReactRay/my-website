417f0ec493b76baa1fed338dffa7e04f17f5a713
ReactRay <raydiaz1899@gmail.com>
2025-04-15 15:19:22 +0300

    little changes

---
+++ frontend/src/services/projects.js
@@ -0,0 +1,66 @@
+
+
+export const prj =[
+    {
+      id: '/someday',
+      name: "Someday (Monday Clone)",
+      description: "A task management app inspired by Monday.com. Features Kanban boards, draggable tasks, and group management.",
+      technologies: ["React", "Redux", "Express", "Sockets io", "MongoDB"],
+      img: "/someday.png",
+      github: 'https://github.com/ReactRay/SomeDay',
+      demo: 'https://someday-n1ze.onrender.com',
+      videoId: '742b46be33484fa8880d1c4afe75181b',
+      imgArr: ['https://res.cloudinary.com/danlxus36/image/upload/v1743385875/someday3_mkqprj.png', 'https://res.cloudinary.com/danlxus36/image/upload/v1743385875/someday1_jtut0n.png', 'https://res.cloudinary.com/danlxus36/image/upload/v1743385875/someday4_bnzz8p.png']
+    },
+    {
+      id: '/chatify',
+      name: "Chatify (Spotify Clone + Chat)",
+      description: "a real-time music streaming web app — a modern Spotify-style clone that allows users to listen to music, chat live, and see what others are playing. Admins can upload albums and tracks to be shared across all users. This app does not use Spotify or YouTube APIs — all music is hosted and managed directly.",
+      technologies: ["React", "Zustand", "Express", "Sockets io", "MongoDB", "Mongoose", "Clerk",],
+      img: "/chatify.png",
+      github: 'https://github.com/ReactRay/Chatify',
+      demo: 'https://chatify-na41.onrender.com',
+      video: "https://res.cloudinary.com/danlxus36/video/upload/v1744718299/chaitfy_iwblmy.mp4",
+      imgArr: ['https://res.cloudinary.com/danlxus36/image/upload/v1744712597/chatify_c0lu2l.png',
+        'https://res.cloudinary.com/danlxus36/image/upload/v1744712597/chatify3_dcyp2r.png'
+        , 'https://res.cloudinary.com/danlxus36/image/upload/v1744712597/chatify4_bsfppn.png']
+    },
+    {
+      id: '/talkyWocky',
+      name: "TalkyWocky",
+      description: "A chat app with responsive layout and a wide array of themes to choose from , providing quick live chat supporting text and image transfer.",
+      technologies: ["React", "Express", "Sockets io", "MongoDB", "Mongoose"],
+      img: "/chatty2.png",
+      github: 'https://github.com/ReactRay/chatty',
+      demo: 'https://chatty-8q6m.onrender.com',
+      videoId: '376c5b19c5ea4a12bf4d8926f9e1c71f',
+      imgArr: ['https://res.cloudinary.com/danlxus36/image/upload/v1744071823/chatty1_pgoytq.png', 'https://res.cloudinary.com/danlxus36/image/upload/v1744071823/chatty5_zphy48.png',
+        'https://res.cloudinary.com/danlxus36/image/upload/v1744071823/chatty3_qrdvxy.png']
+
+    },
+    {
+      id: '/missBugs',
+      name: "MissBugs",
+      description: "A bug tracking system where users can report, update, and manage bugs. Full CRUD with a clean UI.",
+      technologies: ["React", "Express", "MongoDB"],
+      img: "/bugs1.png",
+      github: 'https://github.com/ReactRay/bug-report-project',
+      demo: 'https://bug-report-project.onrender.com',
+      videoId: 'ac20b5837e1a41679e2cb40cf75130cb',
+      imgArr: ['https://res.cloudinary.com/danlxus36/image/upload/v1743385876/bugs1_c8cocn.png', 'https://res.cloudinary.com/danlxus36/image/upload/v1743385876/bugs3_ovnqwy.png', 'https://res.cloudinary.com/danlxus36/image/upload/v1743385875/bugs2_yptvdd.png']
+
+    },
+    {
+      id: '/todos',
+      name: "Todos",
+      description: "A simple todo app with full CRUD functionality. Built to help users stay productive and organized.",
+      technologies: ["React"],
+      img: "/todo.png",
+      github: 'https://github.com/ReactRay/RM-TODOS',
+      demo: 'https://reactray.github.io/RM-TODOS/',
+      videoId: '7b718b62ec1848f0918033f026ee61b9',
+      imgArr: ['https://res.cloudinary.com/danlxus36/image/upload/v1743385875/todo1_o0xm00.png', 'https://res.cloudinary.com/danlxus36/image/upload/v1743385875/todo3_wewj8w.png', 'https://res.cloudinary.com/danlxus36/image/upload/v1743385875/todo2_ojvlfr.png']
+
+
+    }
+  ];
